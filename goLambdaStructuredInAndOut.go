9621b31d14e06a8abf376279d7f3d2a4f97bd796
David Cyr <dwcyr@yahoo.com>
2018-03-02 22:03:19 +0000

    add TODO on mystery re output json mapping

--- goLambdaStructuredInAndOut.go
+++ goLambdaStructuredInAndOut.go
@@ -15,12 +15,16 @@ type MyEvent struct {
 	Age  int    "json:age"
 }
 
+// TODO - investigate
+// BIT OF A MYSTERY HERE -- 
+// why is the json:code / json:Answer mapping not working for output??
 type MyResponse struct {
+	Code string "json:code"
 	Message string "json:Answer"
 }
 
 func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
+	return MyResponse{Code: "200", Message: fmt.Sprintf("%s is %d years old!", event.Name, event.Age)}, nil
 }
 
 func main() {
